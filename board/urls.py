1026049d524c6b7f8e8348154113ce5cd3711371
hyowon3434 <hyowon3434@naver.com>
2024-07-31 16:04:25 +0900

    save

--- board/urls.py
+++ board/urls.py
@@ -6,4 +6,5 @@ app_name = 'board'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:board_id>/', views.detail, name='detail'),
+    #path('create/', views.create_board, name='create_board')
 ]
