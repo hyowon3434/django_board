a4ee383120ce3b4387cb81ae6fd872edfed40dfd
hyowon3434 <hyowon3434@naver.com>
2024-07-31 15:51:39 +0900

    네임스페이스

--- board/urls.py
+++ board/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'board'
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('<int:board_id>/', views.detail, name='detail'),
 ]
--- board/views.py
+++ board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, JsonResponse
 from .models import Board, Comment
 import json
@@ -6,9 +6,14 @@ import json
 # Create your views here.
 def index(request):
     
-    board_list = Board.objects.order_by('createdAt')
+    board_list = Board.objects.order_by('board_id')
     data = list(board_list.values())
     print(data[0])
     return HttpResponse(data)
     #return render(request, 'board/board_list.html', context)
     #return JsonResponse(data, safe=False)
+
+def detail(request, board_id):
+    board = get_object_or_404(Board, pk=board_id)
+    context = {'board': board}
+    return render(request, 'board/board_detail.html', context)
