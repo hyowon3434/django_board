fd62370e365d8caa52dc4f5ca7adc7be3e4234d4
hyowon3434 <hyowon3434@naver.com>
2024-07-31 14:09:28 +0900

    model migrated

--- board/models.py
+++ board/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Board(models.Model):
+    board_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    writer_id = models.CharField(max_length=50)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    
+class Comment(models.Model):
+    commend_id = models.AutoField(primary_key=True)
+    board_id = models.ForeignKey(Board, on_delete=models.CASCADE)
+    content = models.TextField()
+    writer_id = models.CharField(max_length=50)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
